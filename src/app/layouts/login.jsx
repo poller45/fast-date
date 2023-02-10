f403d629a0b4006ac799e7b7e45052cb8d65c310
Pavlo Cherniakov <panya382@gmail.com>
2023-02-10 11:18:12 +0200

    make validation for email and password

--- src/app/layouts/login.jsx
+++ src/app/layouts/login.jsx
@@ -1,30 +1,51 @@
 import React, { useState } from 'react';
 import { useEffect } from 'react';
 import TextField from '../components/textField';
+import { validator } from "../utils/validator";
 
 const Login = () => {
-   const [data, setData] = useState({ email: '', password: '' });
-   const [errors, setErrors] = useState({});
-   const handleChange = ({ target }) => {
-      setData((prevState) => ({
-         ...prevState, [target.name]: target.value
-      }));
-   };
-
-   useEffect(() => {
-      validate();
-   }, [data])
-
-   const validate = () => {
-      const errors = {};
-      for (const fieldName in data) {
-         if (data[fieldName].trim() === "") {
-            errors[fieldName] = `${fieldName} обязательно для заполнения`;
-         }
-      }
-      setErrors(errors);
-   };
-  //const isValid = Object.keys(errors).length === 0;
+    const [data, setData] = useState({ email: "", password: "" });
+    const [errors, setErrors] = useState({});
+    const handleChange = ({ target }) => {
+        setData((prevState) => ({
+            ...prevState,
+            [target.name]: target.value
+        }));
+    };
+    const validatorConfig = {
+        email: {
+            isRequired: {
+                message: "Электронная почта обязательна для заполнения"
+            },
+            isEmail: {
+                message: "Email введен некорректно"
+            }
+        },
+        password: {
+            isRequired: {
+                message: "Пароль обязателен для заполнения"
+            },
+            isCapitalSymbol: {
+                message: "Пароль должен содержать хотя бы одну заглавную букву"
+            },
+            isContainDigit: {
+                message: "Пароль должен содержать хотя бы одно число"
+            },
+            min: {
+                message: "Пароль должен состоять минимум из 8 символов",
+                value: 8
+            }
+        }
+    };
+    useEffect(() => {
+        validate();
+    }, [data]);
+    const validate = () => {
+        const errors = validator(data, validatorConfig);
+        setErrors(errors);
+        return Object.keys(errors).length === 0;
+    };
+    const isValid = Object.keys(errors).length === 0;
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -32,26 +53,38 @@ const Login = () => {
         if (!isValid) return;
         console.log(data);
     };
-   
-   return (
-      <form onSubmit={handleSubmit}>
-         <TextField
-            label='E - mail'
-            name='email'
-            value={data.email}
-            onChange={handleChange}
-            error={errors.email}/>
-         <TextField
-            label='Passwodr'
-            type='password'
-            name='password'
-            value={data.password}
-            onChange={handleChange}
-            error={errors.password}/>
-         <button type='submit'>Submit</button>
-                  
-   </form>
-   )
-}
- 
+    return (
+        <div className="container mt-5">
+            <div className="row">
+                <div className="col-md-6 offset-md-3 shadow p-4">
+                    <h3 className="mb-4">Login</h3>
+                    <form onSubmit={handleSubmit}>
+                        <TextField
+                            label="Электронная почта"
+                            name="email"
+                            value={data.email}
+                            onChange={handleChange}
+                            error={errors.email}
+                        />
+                        <TextField
+                            label="Пароль"
+                            type="password"
+                            name="password"
+                            value={data.password}
+                            onChange={handleChange}
+                            error={errors.password}
+                        />
+                        <button
+                            className="btn btn-primary w-100 mx-auto"
+                            type="submit"
+                            disabled={!isValid}
+                        >
+                            Submit
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
 export default Login;
